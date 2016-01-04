da0ada12418242a60f760820f55b242ddd9c1435
Jitendra Gupta <jitender.gupta@walmart.com>
2016-01-04 12:10:42 +0530

    Added some storage addition verifications

--- create_fs.rb
+++ create_fs.rb
@@ -1,5 +1,7 @@
+=begin
+
 Assumptions:-
-  1. This is for a CentOS vm.
+  1. This is for a CentOS VM.
   2. The VM is up and running.
   3. The new storage device is already created in Azure and is attached to the VM.
 
@@ -9,36 +11,70 @@ This script does the following steps :-
   3. Creates filesystem(ext4) by default on the new device.
   4. Mounts it in the specified mountpoint.
 
-Jgupta4StorageTestVM1-20151222-115611.vhd
+azure_attach_blob "app1" do
+  "device": "/dev/sdc",
+  "fstype": "ext4",
+  "mountpoint": "/mnt",
+  "user": "root",
+  "group": "root",
+  "mode": "777"
+end
 
-class add_storage_device
-  attr_accessor :device, :fstype, :mountpoint, :user, :group, :mode
+=end
+
+class Add_Storage_Device
 
   def initialize(device, fstype, mountpoint, user, group, mode)
     @device, @fstype, @mountpoint, @user, @group, @mode = device, fstype, mountpoint, user, group, mode
   end
 
+  protected
+
+  def attached_to_vm?
+    puts "Device to be verified for attachment = #{@device}"
+    true
+  end
+
+  def is_mounted?
+    puts "Device to be verified for mount = #{@device}"
+    false
+  end
+
+  def fs_exists?
+    puts "Device to be verified for fs_exists = #{@device} for FS type #{@fstype}"
+    false
+  end
+
+  def create_mountpoint?
+    puts "mountpoint to be created on #{@mountpoint}"
+    true
+  end
+
+  def create_fs?
+    puts "File system #{@fstype} to be created on #{@device}"
+    true
+  end
+
+  def mount_fs?
+    puts "Device #{@device} to be mounted on #{@mountpoint}"
+    true
+  end
+
+  public
+
+  def verify_storage_disk?
+    if self.attached_to_vm? && !self.is_mounted? && !self.fs_exists?
+          if self.create_fs? && self.create_mountpoint? && self.mount_fs?
+                true
+          end
+    end
+  end
 
 end
 
-            "device": "/dev/sdc",
-            "fstype": "ext4",
-            "mountpoint": "/mnt",
-            "user": "root",
-            "group": "root",
-            "mode": "777"
-
-if device_attached_to_vm("#{device}")
-  if device_not_mounted("#{device}")
-    if fs_exists("#{device}")
-      create_fs("#{device}")
-      mount_fs("#{device}")
-    else
-      puts "ERROR -  Filesystem already exists on Storage device #{device}"
-    end
-  else
-    puts "Error - Storage device #{device} is currently mounted and in use."
-  end
+app1_fs = Add_Storage_Device.new("/dev/sdc", "ext4", "/app1", "app1", "app1", "755")
+if app1_fs.verify_storage_disk?
+  puts " SUCCESS - Device attached "
 else
-  puts "ERROR - Storage device #{device} is not attached to VM."
+  puts " ERROR - Failed to attach the device"
 end
